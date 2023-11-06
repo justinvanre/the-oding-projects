ff571bfa53cb6ddd166ec83ae486cba3bd5a30d8
Justin Vanre <justin.vr@startmail.com>
2023-11-06 07:49:55 +0100

    Algorithm to play the game
    This is the algorithm to play the game rock paper scissors. We require the user
    to select 1 of the 3 choices and return
    a random choice for the computer the user
    is playing against. With these 2 choices we determine the winner.

---
+++ rock-paper-scissors/app.js
@@ -0,0 +1,35 @@
+const getRandomNum = range => Math.floor(Math.random * range); 
+
+function getComputerChoice(choice) {
+    switch (choice) {
+        case 0: 
+            return 'Rock'; 
+        case 1: 
+            return 'Paper';
+        case 2:
+            return 'Scissors';
+        default:
+            console.log('this is not an option');
+            break;
+    }
+}
+
+function playRound() {
+
+    if (playerSelection === computerSelection) {
+        return 'The game is a tie.';
+
+    } else if ( (playerSelection === 'Rock') && (computerSelection === 'Scissors') ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+
+    } else if ( (playerSelection === 'Paper') && (computerSelection === 'Rock') ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+
+    } else if ( (playerSelection === 'Scissors') && (computerSelection === 'Paper') ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+
+}
